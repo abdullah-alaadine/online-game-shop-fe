122ea549335577684b3323c623ddf6640b1510e6
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-05 18:10:57 +0200

    added the function to get the id of the item that is assigned to each button

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,33 +1,33 @@
 window.onload = function() {
-const images = ['images/top-bg/ps-background.webp', 'images/top-bg/playstation-games-bg.png', 'images/top-bg/gow-bg.jpg'];
-const slide = document.querySelector('.slide img');
-const prev_btn = document.querySelector('.prev');
-const next_btn = document.querySelector('.next');
-let current_image_index = 0;
-let timer;
+const images = ['images/top-bg/ps-background.webp', 'images/top-bg/playstation-games-bg.png', 'images/top-bg/gow-bg.jpg']
+const slide = document.querySelector('.slide img')
+const prev_btn = document.querySelector('.prev')
+const next_btn = document.querySelector('.next')
+let current_image_index = 0
+let timer
 
 function show_image(index) {
-  slide.src = images[index];
+  slide.src = images[index]
 }
 
 function prev_image() {
-    clearInterval(timer);
-    current_image_index--;
+    clearInterval(timer)
+    current_image_index--
     if (current_image_index < 0) {
-        current_image_index = images.length - 1;
+        current_image_index = images.length - 1
     }
-    show_image(current_image_index);
-    timer = setInterval(next_image, 3000);
+    show_image(current_image_index)
+    timer = setInterval(next_image, 3000)
 }
 
 function next_image() {
-    clearInterval(timer);
-    current_image_index++;
+    clearInterval(timer)
+    current_image_index++
     if (current_image_index >= images.length) {
-        current_image_index = 0;
+        current_image_index = 0
     }
-    show_image(current_image_index);
-    timer = setInterval(next_image, 3000);
+    show_image(current_image_index)
+    timer = setInterval(next_image, 3000)
 }
 
 prev_btn.addEventListener('click', prev_image)
@@ -49,5 +49,15 @@ function toggle_search_bar() {
         console.log(`Searching for: ${search_input.value}`)
     }
 }
+
+const item_buttons = document.querySelectorAll('.add-to-cart-btn')
+
+item_buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const parentDiv = button.parentNode;
+        const parentId = parentDiv.id;
+        console.log(parentId);
+    })
+})
 
 }
