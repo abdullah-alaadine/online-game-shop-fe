6a9fe5c50d38e1df6ff906abe455e1821894a85f
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-06 15:54:12 +0200

    fixed a problem in the profile page

--- mauserprof/function.js
+++ mauserprof/function.js
@@ -1,58 +1,58 @@
-// Get references to the buttons and sections
-const profileButton = document.getElementById('profile-button');
-const ordersButton = document.querySelector('#orders-button');
-const addressesButton = document.querySelector('#addresses-button');
-const paymentsButton = document.querySelector('#payments-button');
-const adminbutton = document.querySelector('#admin-button');
 
-const profileSection = document.querySelector('#profile-section');
-const ordersSection = document.querySelector('#orders-section');
-const addressesSection = document.querySelector('#addresses-section');
-const paymentsSection = document.querySelector('#payments-section');
-const adminSection = document.querySelector('#admin-section');
+  const profileButton = document.getElementById('profile-button');
+  const ordersButton = document.querySelector('#orders-button');
+  const addressesButton = document.querySelector('#addresses-button');
+  const paymentsButton = document.querySelector('#payments-button');
+  const adminbutton = document.querySelector('#admin-button');
 
-// Add event listeners to the buttons
-profileButton.addEventListener('click', () => {
-  // Hide all sections
-  profileSection.style.display = 'block';
-  ordersSection.style.display = 'none';
-  addressesSection.style.display = 'none';
-  paymentsSection.style.display = 'none';
-  adminSection.style.display ='none';
-});
+  const profileSection = document.querySelector('#profile-section');
+  const ordersSection = document.querySelector('#orders-section');
+  const addressesSection = document.querySelector('#addresses-section');
+  const paymentsSection = document.querySelector('#payments-section');
+  const adminSection = document.querySelector('#admin-section');
 
-ordersButton.addEventListener('click', () => {
-  // Hide all sections
-  profileSection.style.display = 'none';
-  ordersSection.style.display = 'block';
-  addressesSection.style.display = 'none';
-  paymentsSection.style.display = 'none';
-  adminSection.style.display ='none';
-});
+  // Add event listeners to the buttons
+  profileButton.addEventListener('click', () => {
+    // Hide all sections
+    profileSection.style.display = 'block';
+    ordersSection.style.display = 'none';
+    addressesSection.style.display = 'none';
+    paymentsSection.style.display = 'none';
+    adminSection.style.display ='none';
+  });
 
-addressesButton.addEventListener('click', () => {
-  // Hide all sections
-  profileSection.style.display = 'none';
-  ordersSection.style.display = 'none';
-  addressesSection.style.display = 'block';
-  paymentsSection.style.display = 'none';
-  adminSection.style.display ='none';
-});
+  ordersButton.addEventListener('click', () => {
+    // Hide all sections
+    profileSection.style.display = 'none';
+    ordersSection.style.display = 'block';
+    addressesSection.style.display = 'none';
+    paymentsSection.style.display = 'none';
+    adminSection.style.display ='none';
+  });
 
-paymentsButton.addEventListener('click', () => {
-  // Hide all sections
-  profileSection.style.display = 'none';
-  ordersSection.style.display = 'none';
-  addressesSection.style.display = 'none';
-  paymentsSection.style.display = 'block';
-  adminSection.style.display ='none';
-});
+  addressesButton.addEventListener('click', () => {
+    // Hide all sections
+    profileSection.style.display = 'none';
+    ordersSection.style.display = 'none';
+    addressesSection.style.display = 'block';
+    paymentsSection.style.display = 'none';
+    adminSection.style.display ='none';
+  });
 
-adminbutton.addEventListener('click', () => {
+  paymentsButton.addEventListener('click', () => {
     // Hide all sections
     profileSection.style.display = 'none';
     ordersSection.style.display = 'none';
     addressesSection.style.display = 'none';
-    paymentsSection.style.display = 'none';
-    adminSection.style.display ='block';
+    paymentsSection.style.display = 'block';
+    adminSection.style.display ='none';
+  });
+
+  adminbutton.addEventListener('click', () => {
+      // Hide all sections
+      profileSection.style.display = 'none';
+      ordersSection.style.display = 'none';
+      addressesSection.style.display = 'none';
+      paymentsSection.style.display = 'none';
+      adminSection.style.display ='block';
   });
