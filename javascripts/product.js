f9233fc891b1229ead8205a573f0672db0319c67
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-06 15:44:57 +0200

    changed the sign in / sign up style

--- javascripts/product.js
+++ javascripts/product.js
@@ -1,8 +1,8 @@
 window.onload = function(){
-    
+
     function updateProduct(src, name, pr, description) {
-        var img = document.querySelector(".product_container img")
-        var title = document.querySelector(".product_container .title h1")
+        let img = document.querySelector(".product_container img")
+        let title = document.querySelector(".product_container .title h1")
         var price = document.querySelector(".product_container .price h2")
         var desc = document.querySelector(".product_container .desc")
       
@@ -11,5 +11,6 @@ window.onload = function(){
         price.textContent = pr
         desc.textContent = description
     }
-    updateProduct("images/ps4-games/fifa.webp", "fifa","500","hello I am fifa")
+
+
 }
