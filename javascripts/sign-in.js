6153172271fba8e8f3c7081ae6541f6f3dda0265
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-06 18:00:25 +0200

    signin api added

--- javascripts/sign-in.js
+++ javascripts/sign-in.js
@@ -20,7 +20,7 @@ window.onload = function() {
 
         let url = "http://localhost:8000/api/login";
         axios.post(url, data).then( function(res){
-            //--> ! <--
+            location.replace("./index.html");
         }).catch(err => {
             alert(err.response.data.message);
         })
